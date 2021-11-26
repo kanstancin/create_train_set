99bca3d4c36effe61a8f32eaa31a8c8dcc58fdc5
kanstancin <ceitmo@gmail.com>
2021-11-26 18:04:04 -0500

    updates

--- configure_dirs.sh
+++ configure_dirs.sh
@@ -3,3 +3,6 @@ mkdir -p data/dataset_out/images/train
 mkdir -p data/dataset_out/images/val
 mkdir -p data/dataset_out/labels/train
 mkdir -p data/dataset_out/labels/val
+
+rm -rf data/spaghetti_proc
+mkdir data/spaghetti_proc
