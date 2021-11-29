7442f99bb5d8489e195e42a705463d32b95b3bec
kanstancin <ceitmo@gmail.com>
2021-11-29 16:19:52 -0500

    add display

---
+++ display_imgs.py
@@ -0,0 +1,26 @@
+import matplotlib.pyplot as plt
+from mpl_toolkits.axes_grid1 import ImageGrid
+import numpy as np
+import random
+import cv2
+from glob import glob
+from tqdm import tqdm
+
+files = glob("data/detection_out/*")
+for _ in range(1):
+    row = 3
+    col = 3
+    grid_files = random.sample(files, row*col)
+    images     = []
+    for image_path in tqdm(grid_files):
+        img          = cv2.cvtColor(cv2.imread(image_path), cv2.COLOR_BGR2RGB)
+        dim = (500, int(500 * img.shape[0] / img.shape[1]))
+        # resize image
+        img = cv2.resize(img, dim, interpolation=cv2.INTER_AREA)
+        images.append(img)
+    fig, axes = plt.subplots(col, row)
+    for i, img in enumerate(images):
+        axes[i//col, i%col].imshow(img)
+        axes[i // col, i % col].axis('off')
+    fig.tight_layout()
+    plt.show()
