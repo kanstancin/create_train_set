e51fc3e1ccbc288b1c791fe4cec8c41a4769aabd
kanstancin <ceitmo@gmail.com>
2022-01-10 12:04:32 -0500

    no blur

--- preprocess_test_set.py
+++ preprocess_test_set.py
@@ -12,13 +12,13 @@ path_out = "data/spaghetti_proc"
 im_names = get_img_paths(path_inp)
 for im_name in im_names:
     img = cv.imread(path_inp + "/" + im_name, 1)
-    dim = (1000, int(1000 * img.shape[0]/img.shape[1]))
-    # resize image
-    img = cv.resize(img, dim, interpolation=cv.INTER_AREA)
-    img = cv.GaussianBlur(img, (3, 3), cv.BORDER_DEFAULT)
+    # dim = (1000, int(1000 * img.shape[0]/img.shape[1]))
+    # # resize image
+    # img = cv.resize(img, dim, interpolation=cv.INTER_AREA)
+    # img = cv.GaussianBlur(img, (3, 3), cv.BORDER_DEFAULT)
     path_out_full = path_out + "/" + im_name
 
-    img = cv.cvtColor(img, cv.COLOR_RGB2GRAY)
+    img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
     img = [img, img, img]
     img = np.transpose(img, (1, 2, 0))
     cv.imwrite(path_out_full, img)
