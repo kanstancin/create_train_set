2d5e053174d0c07ed95a8701624b5a14cf8e8ace
kanstancin <ceitmo@gmail.com>
2022-01-12 17:49:58 -0500

    prep

--- preprocess_test_set.py
+++ preprocess_test_set.py
@@ -12,10 +12,10 @@ path_out = "data/spaghetti_proc"
 im_names = get_img_paths(path_inp)
 for im_name in im_names:
     img = cv.imread(path_inp + "/" + im_name, 1)
-    # dim = (1000, int(1000 * img.shape[0]/img.shape[1]))
-    # # resize image
-    # img = cv.resize(img, dim, interpolation=cv.INTER_AREA)
-    # img = cv.GaussianBlur(img, (3, 3), cv.BORDER_DEFAULT)
+    dim = (1000, int(1000 * img.shape[0]/img.shape[1]))
+    # resize image
+    img = cv.resize(img, dim, interpolation=cv.INTER_AREA)
+    img = cv.GaussianBlur(img, (5, 5), cv.BORDER_DEFAULT)
     path_out_full = path_out + "/" + im_name
 
     img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
